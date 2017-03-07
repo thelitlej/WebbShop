11f71f2d953309d50e4dcebd340e5ade5afd673a
thelitlej <jonasgustafson@hotmail.se>
2017-03-07 20:14:15 +0100

    Fixat password. Lägg bara till följande: "[Password] NVARCHAR (50) NOT NULL" i usertabellen i databasen och hämta sen från min branch

--- Webbshop/Webbshop/Controllers/UsersController.cs
+++ Webbshop/Webbshop/Controllers/UsersController.cs
@@ -46,7 +46,7 @@ namespace Webbshop.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,First_Name,Last_Name,Phone_number,Email,Address,Postal_Code,City,Country")] User user)
+        public ActionResult Create([Bind(Include = "Id,First_Name,Last_Name,Phone_number,Email,Address,Postal_Code,City,Country,Password")] User user)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace Webbshop.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,First_Name,Last_Name,Phone_number,Email,Address,Postal_Code,City,Country")] User user)
+        public ActionResult Edit([Bind(Include = "Id,First_Name,Last_Name,Phone_number,Email,Address,Postal_Code,City,Country,Password")] User user)
         {
             if (ModelState.IsValid)
             {
