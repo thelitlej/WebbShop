c8da62203a36dcca8a2dfaa53931360f769781cc
alexn <alexnorrman@gmail.com>
2017-03-08 10:50:07 +0100

    added

--- Webbshop/Webbshop/Models/User.cs
+++ Webbshop/Webbshop/Models/User.cs
@@ -29,6 +29,7 @@ namespace Webbshop.Models
         public string Postal_Code { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string Password { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order> Order { get; set; }
