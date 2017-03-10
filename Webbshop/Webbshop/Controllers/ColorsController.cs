84a0ad9ad64af3c450a8d5f4f351cdfd3ad91269
alexn <alexnorrman@gmail.com>
2017-03-10 15:12:28 +0100

    Fixing upload not done

--- Webbshop/Webbshop/Controllers/ColorsController.cs
+++ Webbshop/Webbshop/Controllers/ColorsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -49,17 +50,31 @@ namespace Webbshop.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Product_Id,Color1,Img_Name")] Color color)
+        public ActionResult Create([Bind(Include = "Id,Product_Id,Color1")] Color color, HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+            var allowedExtensions = new[] { ".Jpg", ".png", ".jpg", "jpeg" };
+            string url = file.ToString(); //getting complete url  
+            var fileName = Path.GetFileName(file.FileName); //getting only file name(ex-ganesh.jpg)  
+            var ext = Path.GetExtension(file.FileName); //getting the extension(ex-.jpg)  
+            string img_name = fileName.ToString();
+            color.Img_Name = img_name;
+            if (ModelState.IsValid && allowedExtensions.Contains(ext)) //check what type of extension  
             {
+                string name = Path.GetFileNameWithoutExtension(fileName); //getting file name without extension  
+                string myfile = name + ext; //appending the name with id  
+                                            // store the file inside ~/project folder(Img)  
+                var path = Path.Combine(Server.MapPath("~/App_Data"), myfile);
+                //color.Img_Name = path;
                 db.Color.Add(color);
                 db.SaveChanges();
-
+                file.SaveAs(path);
                 TempData["Color"] = "Färgen " + color.Color1.ToLower() + " har lagts till.";
                 return RedirectToAction("Create");
             }
-
+            else
+            {
+                ViewBag.message = "Please choose only Image file";
+            }
             ViewBag.Product_Id = new SelectList(db.Product, "Id", "Name", color.Product_Id);
             return View(color);
         }
