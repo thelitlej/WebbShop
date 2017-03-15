8621970d3d8ee3101a8f65eb4e5b820f33d1ecaf
Hanna <Hanna@DESKTOP-VIINSPV>
2017-03-15 11:02:38 +0100

    lösenord färdigt

--- Webbshop/Webbshop/Controllers/ViewUserController.cs
+++ Webbshop/Webbshop/Controllers/ViewUserController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Webbshop.Models;
@@ -20,6 +21,9 @@ namespace Webbshop.Controllers
         [HttpPost]
         public ActionResult Index(ViewUser vu)
         {
+            String pass = vu.Password;
+            vu.Password = GenerateSHA256Hash(pass).Substring(14);
+
             if (!String.IsNullOrEmpty(vu.Email) && !String.IsNullOrEmpty(vu.Password))
             {
                 foreach (User i in db.User)
@@ -40,6 +44,21 @@ namespace Webbshop.Controllers
                 }
             }
             return View();
+        }
+
+        public string GenerateSHA256Hash(String password)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(password);
+            System.Security.Cryptography.SHA256Managed sha256hashstring = new System.Security.Cryptography.SHA256Managed();
+            byte[] hash = sha256hashstring.ComputeHash(bytes);
+
+            StringBuilder hexString = new StringBuilder(hash.Length * 2);
+            foreach (byte b in hash)
+            {
+                hexString.AppendFormat("{0:x2}", b);
+            }
+
+            return hexString.ToString();
         }
     }
 }
